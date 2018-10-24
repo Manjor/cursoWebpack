88bb756fe5339da60416d147d13160e641cd91e8
Manoel <manoeltavares.jr@gmail.com>
2018-10-24 10:25:55 -0300

    Estrutura Basica do Projeto

---
+++ webpack/webpack.config.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack')
+
+
+module.exports = {
+    //Modo de Desenvolvimento
+    mode: 'development',
+    //Entrada raiz, serpá principal.js nesse modulo
+    entry: './src/principal.js'
+}
