ae3b7cd4e38e819db06541f694550f42a24edabb
manjor <manoeltavares.jr@gmail.com>
2018-10-25 15:47:39 -0300

    Compressão de Css e Ambiente Desenvolvimento&Producao

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,19 @@
+//modoDev seta a variavel de ambiente
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
+//MiniCssEx... extrair o css para um arquivo separado do js principal
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//UglifyPlugin realiza a minimificacao
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+//Optimize realiza a minimificacao do css
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 //Exporta o objeto que representa toda a configuração do webpack
 module.exports = {
     //Modo de Desenvolvimento
     //mode: production
     //no modo producao irá realizar a minimacao do codigo main.js na pasta dist
-    mode: 'development',
+    mode: modoDev ? 'development':'production',
     //Entrada raiz, serpá principal.js nesse modulo
     entry: './src/principal.js',
     //Configura o nome e pasta do(s) arquivo(s) de saída
@@ -15,6 +22,15 @@ module.exports = {
         filename: 'principal.js',
         //pasta destino do arquivo de saída
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer:[
+            new UglifyPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     //Define os Plugins para a aplicação
     plugins: [
